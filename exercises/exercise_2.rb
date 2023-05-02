0f319089b740331313d77f69b7c9d35e9639e170
Kevin Cho <kevcho604@gmail.com>
2023-05-02 14:48:08 -0700

    Assign instance variables to stores and change name of store 1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,25 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
+=begin
+Exercise 2: Update the first store
+* Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+* Load the second store from the database and assign it to @store2.
+* Update the first store (@store1) instance in the database. (Change its name or something.)
+=end
+
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: '1') 
+
+@store2 = Store.find_by(id: '2') 
+
+@store1.name = "Vancouver" 
+@store1.save 
+
+# Testing
+# puts @store1.name
+# puts @store2.name
