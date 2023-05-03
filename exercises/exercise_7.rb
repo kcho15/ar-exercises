9e52dc28995607e057cd60159620adbee2f965e6
Kevin Cho <kevcho604@gmail.com>
2023-05-02 18:02:15 -0700

    Exercise adding validation and error message

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -5,8 +5,41 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
+### Exercise 7: Validations for both models
+=begin
+1. Add validations to two models to enforce the following business rules:
 
+- Employees must always have a first name present
+- Employees must always have a last name present
+- Employees have a hourly_rate that is a number (integer) between 40 and 200
+- Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+
+- Stores must always have a name that is a minimum of 3 characters
+- Stores have an annual_revenue that is a number (integer) that must be 0 or more
+- BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
+
+2. Ask the user for a store name (store it in a variable)
+3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+=end 
+
+puts "----------"
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, :last_name, :store_id, presence: true 
+  validates :hourly_rate, numericality: { in: 40..200 }
+end 
+
+class Store 
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 } 
+end 
+
+puts "Give me a store name >"
+print ">"
+@input = gets.chomp 
+store = Store.create(name: "#{@input}")
+puts store.errors.messages 
