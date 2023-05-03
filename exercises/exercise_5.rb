e4215f7af7ea260c5513fc269849e2cae87b4cc3
Kevin Cho <kevcho604@gmail.com>
2023-05-02 17:42:42 -0700

    Exercise using sum and average, and chaining where and count methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,22 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+=begin
+### Exercise 5: Calculations
+
+1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+2. On the next line, also output the average annual revenue for all stores.
+3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+=end
+
+puts "----------"
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue) 
+puts Store.average(:annual_revenue)
+
+one_mil_sales_stores = Store.where('annual_revenue > ?', 1000000).count
+
+puts one_mil_sales_stores
